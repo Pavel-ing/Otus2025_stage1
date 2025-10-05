436a441e23c1c63518c6d2c3ce0eb5f411fc954d
Pavel_D <Vegas3@inbox.ru>
2025-10-05 18:50:16 +0300

    done check this)

---
+++ draft.py
@@ -0,0 +1,25 @@
+#from collections.abc import async_generator
+
+import sys
+sys.platform
+print(sys.version)
+# class Human:
+#     def __init__(self, gender: str, age: int):
+#         self.gender = gender
+#         self.age = age
+#
+# h = Human (gender='W', age=35)
+# print(h.gender)
+# print(h.age)
+
+# x = int(input('Введите число:'))
+# if x == 5:
+#     print("Ты угадал")
+# else: print("Попробуй ещё")
+
+import struct
+
+# a = ".... vasya .....  "
+# b = a.strip(".")
+# b = a.strip()
+# print(b.title())
