e7da6b8b626f85b600d1e0bad6fa5c3da0ab8a07
Pavel_D <Vegas3@inbox.ru>
2025-10-12 20:19:51 +0300

    ..

---
+++ src/circle.py
@@ -0,0 +1,23 @@
+from figure import Figure
+from src.triangle import Triangle
+
+
+class Circle(Figure):
+    def __init__(self,r):
+        if r <= 0:
+            raise ValueError ("Radius of a circle can't be less or equal than 0 !")
+        self.r = r
+
+    @property
+    def perimeter(self):
+        return 2 * 3.14 * self.r
+
+    @property
+    def area(self):
+        return 3.14 * self.r ** 2
+
+c = Circle(10)
+print(c.area)
+t = Triangle(13,14,15)
+print(t.area)
+print(t.add_area(c))
