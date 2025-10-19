a77adbdd7350291f9a164696a90723933ad129a5
Pavel-ing <Vegas3@inbox.ru>
2025-10-19 13:06:38 +0300

    Delete src directory

--- src/square.py
+++
@@ -1,7 +0,0 @@
-from rectangle import Rectangle
-
-class Square(Rectangle):
-    def __init__(self, a):
-        if a <= 0 :
-            raise ValueError("Sides can't be less or equal than 0 ! ")
-        super().__init__(a,a)
